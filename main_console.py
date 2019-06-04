65410ca7b83485c41516f274ba10e36bcf549a24
yadaniel <ya.daniel@inbox.ru>
2019-06-04 18:01:11 +0200

    beep added

--- main_console.py
+++ main_console.py
@@ -3,6 +3,12 @@
 
 import sys, serial, struct, time, atexit, argparse
 
+# platform dependent code
+import platform
+print platform.system()
+if platform.system().lower() in ["windows"]:
+    from winsound import Beep as beep
+
 parser = argparse.ArgumentParser(description="SDS011 PM2.5 PM10 measurement")
 parser.add_argument("com", help="com port")
 parser.add_argument("--cont", action="store_true", help="cont. mode")
@@ -15,6 +21,11 @@ try:
 except Exception as exp:
     print(exp)
     sys.exit()
+
+def as_hex(bs, n=0):
+    import binascii
+    sys.stdout.write(" "*n + binascii.hexlify(bs) + "\n")
+    sys.stdout.flush()
 
 def sensor_wake():
     bs = [
@@ -40,37 +51,47 @@ def sensor_wake():
     for b in bs:
         ser.write(b)
 
-def process_frame(d):
-    r = struct.unpack('<HHxxBB', d[2:]) # < = little endian, H = unsigned short, x = pad byte, B = unsigned char
+def parse_data(data):
+    r = struct.unpack('<HHxxBB', data[2:]) # < = little endian, H = unsigned short, x = pad byte, B = unsigned char
     pm25 = r[0]/10.0
     pm10 = r[1]/10.0
-    checksum = sum(ord(v) for v in d[2:8]) % 0xFF
-    print("PM 2.5: {:3} ug/m^3  PM 10: {:3} ug/m^3 CRC={:3}".format(pm25, pm10, "OK" if (checksum==r[2] and r[3]==0xAB) else "NOK"))
+    checksum = sum(ord(v) for v in data[2:8]) % 256
+    if r[2] == checksum and r[3] == 0xAB:
+        return True, pm25, pm10
+    else:
+        return False, 0, 0
+
+def process_frame(pm25, pm10):
+    print("PM 2.5: {:3} ug/m^3  PM 10: {:3} ug/m^3".format(pm25, pm10))
     sys.stdout.flush()
     return pm25, pm10
 
+def process_beep(pm25, pm10):
+    if pm25*10 >= 30 or pm10*10 >= 40:
+        for _ in range(3):
+            beep(2500, 500)
+    elif pm25*10 >= 20 or pm10*10 >= 30:
+        for _ in range(3):
+            beep(2500, 100)
+    return pm25, pm10
+
 pm25_max, pm10_max = 0, 0
-def process_frame_stars(d):
+def process_frame_stars(pm25, pm10):
     global pm25_max, pm10_max
-    r = struct.unpack('<HHxxBB', d[2:]) # < = little endian, H = unsigned short, x = pad byte, B = unsigned char
-    pm25 = r[0]
-    pm10 = r[1]
+    pm25 = int(pm25*10)
+    pm10 = int(pm10*10)
     pm25_max = max(pm25, pm25_max)
     pm10_max = max(pm10, pm10_max)
-    checksum = sum(ord(v) for v in d[2:8])%256
-    if checksum == r[2] and r[3] == 0xAB:
-        linewidth = 130
-        spaces = linewidth-pm25-pm10-17-2
-        line = "[PM 2.5]%s>%s<%s[PM 10]" % ('.'*pm25, ' '*spaces, '*'*pm10)
-        lst = list(line)
-        lst[pm25_max+9] = ']'
-        lst[linewidth-pm10_max-11] = '['
-        line = "".join(lst)
-    else:
-        line = "???"
+    linewidth = 130
+    spaces = linewidth-pm25-pm10-17-2
+    line = "[PM 2.5]%s>%s<%s[PM 10]" % ('.'*pm25, ' '*spaces, '*'*pm10)
+    lst = list(line)
+    lst[pm25_max+9] = ']'
+    lst[linewidth-pm10_max-11] = '['
+    line = "".join(lst)
     print(line)
     sys.stdout.flush()
-    return pm25, pm10
+    return pm25/10, pm10/10
 
 def sensor_read():
     while True:
@@ -108,17 +129,25 @@ def sensor_sleep():
 
 if args.cont:
     sensor_wake()
+    time.sleep(3)
     while True:
         data = sensor_read()
-        process_frame_stars(data)
+        valid, pm25, pm10 = parse_data(data)
+        if valid:
+            # process_frame(pm25, pm10)
+            process_frame_stars(pm25, pm10)
+            process_beep(pm25, pm10)
         time.sleep(1)
 else:
     while True:
         sensor_wake()
         time.sleep(10)
         data = sensor_read()
-        # process_frame(data)
-        process_frame_stars(data)
+        valid, pm25, pm10 = parse_data(data)
+        if valid:
+            # process_frame(pm25, pm10)
+            process_frame_stars(pm25, pm10)
+            process_beep(pm25, pm10)
         sensor_sleep()
         time.sleep(20)
 
